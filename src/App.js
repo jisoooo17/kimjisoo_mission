85aa1e80629770ae2578a1aae9520a58ec4c47d6
jisoooo17 <jisoooo17@gmail.com>
2024-08-13 16:40:58 +0900

    map test

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
+import { useEffect, useRef } from 'react'; 
 import './App.css';
 
-function App() {
+function App() {  
+  const mapEl = useRef(null); 
+  const { naver } = window; 
+
+  useEffect(() => { 
+    if (!mapEl.current || !naver) return; 
+    
+    const location = new naver.maps.LatLng(37.5358994, 126.8969627);
+    const mapOptions = {
+      center: location,
+      zoom: 17,
+      zoomControl: true,
+    };
+
+    const map = new naver.maps.Map(mapEl.current, mapOptions);
+    new naver.maps.Marker({
+      position: location,
+      map,
+    });
+  }, []);
+
   return (
     <div className="App">
-      
+      <div className="map" ref={mapEl}></div>
     </div>
   );
 }
